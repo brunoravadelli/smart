8bb3d00e069d85b970086fa1a82231d300eefd95
bruno.090295 <bruno.090295@192.168.3.63>
2014-12-03 18:43:48 -0200

    Fazendo Alterações no App.java

---
+++ src/smartconsertos/SmartConsertos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartconsertos;
+
+/**
+ *
+ * @author bruno.090295
+ */
+public class SmartConsertos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JFrameInicio jInicio = new JFrameInicio();
+        
+    }
+}
